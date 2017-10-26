0d9d26d54ccc3ae967d2b7fc8f76a8ed4d8d71b0
John Tuttle <thejwt@gmail.com>
2017-10-26 01:24:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,6 +20,7 @@ class Application
 
       if @@items.include?(search_term)
         @@cart << search_term
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
